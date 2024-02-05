69e21ec1449d36203d87c0aaa4af070b8f85ac9f
Zac Gladman <Zac.Gladman35@thehutgroup.com>
2024-02-05 16:37:41 +0000

    Improve variable names

--- java/src/main/java/TennisGame1.java
+++ java/src/main/java/TennisGame1.java
@@ -2,35 +2,35 @@ import java.util.Objects;
 
 public class TennisGame1 implements TennisGame {
 
-    private int m_score1 = 0;
-    private int m_score2 = 0;
-    private String player1Name;
-    private String player2Name;
+    private int playerOneScore = 0;
+    private int playerTwoScore = 0;
+    private String playerOneName;
+    private String playerTwoName;
 
-    public TennisGame1(String player1Name, String player2Name) {
-        this.player1Name = player1Name;
-        this.player2Name = player2Name;
+    public TennisGame1(String playerOneName, String playerTwoName) {
+        this.playerOneName = playerOneName;
+        this.playerTwoName = playerTwoName;
     }
 
     public void wonPoint(String playerName) {
         if (Objects.equals(playerName, "player1")) {
-            m_score1 += 1;
+            playerOneScore += 1;
         } else {
-            m_score2 += 1;
+            playerTwoScore += 1;
         }
     }
 
     public String getScore() {
         String score = "";
-        if (m_score1 == m_score2) {
-            score = switch (m_score1) {
+        if (playerOneScore == playerTwoScore) {
+            score = switch (playerOneScore) {
                 case 0 -> "Love-All";
                 case 1 -> "Fifteen-All";
                 case 2 -> "Thirty-All";
                 default -> "Deuce";
             };
-        } else if (m_score1 >= 4 || m_score2 >= 4) {
-            int minusResult = m_score1 - m_score2;
+        } else if (playerOneScore >= 4 || playerTwoScore >= 4) {
+            int minusResult = playerOneScore - playerTwoScore;
             if (minusResult == 1) {
                 score = "Advantage player1";
             } else if (minusResult == -1) {
@@ -41,9 +41,9 @@ public class TennisGame1 implements TennisGame {
                 score = "Win for player2";
             }
         } else {
-            score += getOnePlayersScore(m_score1);
+            score += getOnePlayersScore(playerOneScore);
             score += "-";
-            score += getOnePlayersScore(m_score2);
+            score += getOnePlayersScore(playerTwoScore);
         }
         return score;
     }
